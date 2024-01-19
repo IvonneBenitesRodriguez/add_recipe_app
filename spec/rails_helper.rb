9e18f5b8b72a823a62bf7f5bed914cc1cb216e0d
gilberthappi <gdushimimana6@gmail.com>
2024-01-19 16:46:55 +0200

    fix issue

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,7 +34,6 @@ def login_user
   click_button(class: 'signin-btn', match: :first)
 end
 # Add additional requires below this line. Rails is not loaded until this point!
-
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
