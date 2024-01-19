29f3170c271bfb9b71912ac96928da2cc70edd51
Ivonne <ivonnebenites2015@gmail.com>
2024-01-19 13:33:23 -0500

    Update file

--- db/migrate/20240118120516_create_recipe_foods.rb
+++ db/migrate/20240118120516_create_recipe_foods.rb
@@ -3,10 +3,9 @@ class CreateRecipeFoods < ActiveRecord::Migration[7.1]
     create_table :recipe_foods do |t|
       t.integer :quantity
       t.references :recipe, null: false, foreign_key: true
-      t.references :food, null: false, foreign_key: true
-      
-      t.timestamps
+      t.references :food, null: true, foreign_key: true
 
+      t.timestamps
     end
   end
 end
